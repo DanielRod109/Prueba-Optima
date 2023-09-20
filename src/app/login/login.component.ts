111fbedcc422674bdfbffba0931c34320bb48654
Daniel Rodriguez <dan74109@gmail.com>
2023-09-20 14:20:33 -0500

    Iniciando

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UserLogin } from '../classes/user-login';
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  public myForm!:FormGroup; //Cargar el formulario
+
+  constructor(private fb:FormBuilder,
+    private auth:AuthService,
+    private router: Router){}
+
+
+  ngOnInit(): void {
+      this.myForm = this.createMyForm();
+  }
+
+  private createMyForm():FormGroup{
+    return this.fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    });
+  }
+
+  public submitForm(){
+    const loginData: UserLogin = {
+      username: this.myForm.get('username')?.value,
+      password: this.myForm.get('password')?.value
+    };
+    if(this.myForm.invalid){
+        Object.values(this.myForm.controls).forEach(control=>{
+          control.markAllAsTouched();
+        });
+        return;
+    }else{
+      this.auth.login(loginData).subscribe(
+        data => {
+          console.log(data);
+          if(data.accessToken !=''){
+            console.log("Ingresado")
+            this.router.navigate([''])
+          }
+        },
+        error =>{
+          const Toast = Swal.mixin({
+            toast: true,
+            background: '#DE5454',
+            position: 'top',
+            iconColor: 'white',
+            customClass: {
+              popup: 'colored-toast'
+            },
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true
+          })
+          Toast.fire({
+            icon: 'error',
+            title: 'Credenciales incorrectas'
+          })   
+          console.log(error);
+        }
+      )
+    }
+  }
+
+  public get f():any{
+    return this.myForm.controls;
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { UserLogin } from '../classes/user-login';
+import { Observable, tap } from 'rxjs';
+import { ResponseLogin } from '../classes/response-login';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private urlAuth:string ="http://159.65.96.86:8080/services/auth/signin";
+  private httpHeaders = new HttpHeaders({'Content-Type':'application/json'})
+
+  private isLoggedIn: boolean = false; // Variable que verifica si se ha iniciado sesión
+ 
+  constructor(private http:HttpClient) { }
+
+  login(data:UserLogin): Observable<ResponseLogin>{
+    return this.http.post<ResponseLogin>(this.urlAuth,data).pipe(
+      tap((response:ResponseLogin) => {     
+      if (response.accessToken != ""){
+        this.isLoggedIn = true;
+        }
+      })
+    );
+  }
+
+  logout(){
+    this.isLoggedIn = false;
+  }
+}
